cdb8ea8048183358ab962ae7e000ecf03cbcb611
AndresDiazPinto <andresvis565@gmail.com>
2024-09-26 17:58:28 -0500

    AJUSTES CFA
    Se carga CFA y se genera material

--- src/config/global.js
+++ src/config/global.js
@@ -78,7 +78,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/51250038_CF01_DU.pdf',
       },
       {
         icono: 'fas fa-download',
